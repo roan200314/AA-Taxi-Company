453b18401a5556009df0867f3fcc6f5f0b5748d4
Roan <roan_alhelly@hotmail.com>
2022-12-05 09:21:10 +0100

    css added

--- pagina/index.php
+++ pagina/index.php
@@ -5,15 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/logo.css">
     <title>Fake Taxi: Home</title>
 </head>
 
 <body>
-    <img src="../images/fake_taxi_logo.png" alt="">
-    <?php
-    include "../navbar/navbar.php";
-    ?>
+    <div class="parent">
+        <img id="logo" src="../images/fake_taxi_logo.png" alt="">
+        <?php
+        include "../navbar/navbar.php";
+        ?>
+    </div>
 </body>
 
 </html>
