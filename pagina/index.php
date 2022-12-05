2c99dec5bad0c976480d98e5938f7e23fe925883
samujaxx <samuraijax2003@gmail.com>
2022-12-05 10:56:00 +0100

    jaman

--- pagina/index.php
+++ pagina/index.php
@@ -6,14 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/logo.css">
+    <link rel="stylesheet" href="../css/navbar.css">
     <title>Fake Taxi: Home</title>
 </head>
 
 <body class="parent">
-        <img id="logo" src="../images/fake_taxi_logo.png" alt="">
+    <div class="Navbar">
+        <img id="logo" class="Logo" src="../images/fake_taxi_logo.png" alt="">
         <?php
         include "../navbar/navbar.php";
         ?>
+    </div>
     <div class="Main">
         <div class="title"><h1>Onze Chaffeurs</h1></div>
         <div class="card-1"></div>
