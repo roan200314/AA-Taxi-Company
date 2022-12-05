18d5e843930b2904da3e88ef41c2f518a02f1711
samujaxx <samuraijax2003@gmail.com>
2022-12-05 10:41:01 +0100

    basis homepagina

--- pagina/index.php
+++ pagina/index.php
@@ -5,14 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
     <title>Fake Taxi: Home</title>
 </head>
 
-<body>
-    <?php
-    include "../navbar/navbar.php";
-    ?>
+<body class="parent">
+    <div class="Navbar">
+        <?php
+        include "../navbar/navbar.php";
+        ?>
+    </div>
+    <div class="Main">
+        <div class="title"><h1>Onze Chaffeurs</h1></div>
+        <div class="card-1"></div>
+        <div class="card-2"></div>
+        <div class="card-3"></div>
+        <div class="order"></div>
+    </div>
 </body>
 
 </html>
