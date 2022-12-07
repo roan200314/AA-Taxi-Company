b67b38d9a24699172e30a689bf31b35390e1a7a5
Roan <roan_alhelly@hotmail.com>
2022-12-07 11:27:06 +0100

    homepage fixed

--- pagina/index.php
+++ pagina/index.php
@@ -8,28 +8,26 @@
     <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="../css/logo.css">
     <link rel="stylesheet" href="../css/navbar.css">
-    <link rel="stylesheet" href="../css/navbar.css">
-    <link rel="stylesheet" href="../css/logo.css">
     <title>Fake Taxi: Home</title>
 </head>
 
 <body>
     <div class="parent">
-        <div>
+        <div class="logo">
             <img class="Logo" id="logo" src="../images/fake_taxi_logo.png" alt="">
         </div>
-        <div id="nav-vakje">
-            <?php
-            include "../navbar/navbar.php";
-            ?>
+        <div class="Navbar">
+            <?php include '../navbar/navbar.php' ?>
         </div>
+
+
         <div class="Main">
             <div class="title">
                 <h1>Onze Chauffeurs</h1>
             </div>
-            <div class="card-1"><img id="img1" src="../images/sins.jpg" alt="">
+            <div class="card-1"><img id="img1" src="../images/sins.jpg" alt="" style="border-radius: 25%;">
                 <div class="card-1text">
-                    <p><b>Naam:</b> Johnny Sins</p><br>
+                    <p id="card-naam"><b>Naam:</b> Johnny Sins</p><br>
                     <p><b>Beschrijving:</b> Johnny Sins is een zeer ervaren chauffeur die lang bij ons in dienst is. Zo is Johnny een echte praatjesmaker die ervoor zal zorgen dat jouw rit zeer vermakelijk eindigd.</p>
                     <br>
                     <div class="part">
@@ -38,16 +36,26 @@
                     <button class="button-73" role="button">Bestel</button>
                 </div>
             </div>
-            <div class="card-2"><img id="img2" src="../images/abella.jfif" alt="">
+            <div class="card-2"><img id="img2" src="../images/abella.jfif" alt="" style="border-radius: 25%;">
                 <div class="card-2text">
-                    <p>dadadad</p>
-                    <button>Bestel!</button>
+                    <p id="card-naam"><b>Naam:</b> Abella Danger</p><br>
+                    <p><b>Beschrijving:</b> Abella Danger is een zeer ervaren chauffeur die lang bij ons in dienst is. Zo is Abella een echte praatjesmaker die ervoor zal zorgen dat jouw rit zeer vermakelijk eindigd.</p>
+                    <br>
+                    <div class="part">
+                        <div class="stars" data-percent="60"></div><br>
+                    </div>
+                    <button class="button-73" role="button">Bestel</button>
                 </div>
             </div>
-            <div class="card-3"><img id="img3" src="../images/mia.jpg" alt="">
+            <div class="card-3"><img id="img3" src="../images/mia.jpg" alt="" style="border-radius: 25%;">
                 <div class="card-3text">
-                    <p>dadadad</p>
-                    <button>Bestel!</button>
+                    <p id="card-naam"><b>Naam:</b> Mia Khalifa</p><br>
+                    <p><b>Beschrijving:</b> Mia Khalifa is een zeer ervaren chauffeur die lang bij ons in dienst is. Zo is Mia een echte praatjesmaker die ervoor zal zorgen dat jouw rit zeer vermakelijk eindigd.</p>
+                    <br>
+                    <div class="part">
+                        <div class="stars" data-percent="88"></div><br>
+                    </div>
+                    <button class="button-73" role="button">Bestel</button>
                 </div>
             </div>
             <div class="order"></div>
