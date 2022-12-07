1703a858ad8faa0d0330111bf09514bb333f93f3
Jason <jason.jpy.wong@gmail.com>
2022-12-07 09:18:07 +0100

    Grid veranderd

--- pagina/index.php
+++ pagina/index.php
@@ -6,28 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/logo.css">
     <title>Fake Taxi: Home</title>
 </head>
 
 <body>
     <div class="parent">
-    <div>
-        <img class="Logo" id="logo" src="../images/fake_taxi_logo.png" alt="">
-    </div>
+        <div>
+            <img class="Logo" id="logo" src="../images/fake_taxi_logo.png" alt="">
+        </div>
         <div id="nav-vakje">
-        <?php
-        include "../navbar/navbar.php";
-        ?>
+            <?php
+            include "../navbar/navbar.php";
+            ?>
         </div>
-    <div class="Main">
-        <div class="title">
-            <h1>Onze Chauffeurs</h1>
+        <div class="Main">
+            <div class="title">
+                <h1>Onze Chauffeurs</h1>
+            </div>
+            <div class="card-1"><img src="../images/sins.jpg" alt=""></div>
+            <div class="card-2"></div>
+            <div class="card-3"></div>
+            <div class="order"></div>
         </div>
-        <div class="card-1"><img src="../images/sins.jpg" alt=""></div>
-        <div class="card-2"></div>
-        <div class="card-3"></div>
-        <div class="order"></div>
-    </div>
     </div>
 </body>
 
