29dc92f92c3c95b8507756b3b0e9d8f52a1fc234
Roan <roan_alhelly@hotmail.com>
2022-12-13 10:56:41 +0100

    registreren

--- forms/registreren.php
+++ forms/registreren.php
@@ -16,7 +16,7 @@
 
 <body>
 
-<div class="parent">
+    <div class="parent">
         <div class="logo">
             <img class="Logo" id="logo" src="../images/fake_taxi_logo.png" alt="">
         </div>
@@ -24,13 +24,41 @@
             <?php include '../navbar/navbar.php' ?>
         </div>
     </div>
-    <div class="main" style="display: none;">
+    <div class="main">
         <div class="box">
             <div class="form">
-                Sign up
+                <form action="../behandel/registrerenCheck.php" method="post">
+
+
+                    <label for="Voornaam"><b>Voornaam</b></label>
+                    <input type="text" placeholder="Vul Voornaam in.." name="voornaam" required><br>
+
+                    <label for="achternaam"><b>achternaam</b></label>
+                    <input type="text" placeholder="Vul achternaam in.." name="achternaam" required><br>
+
+                    <label for="email"><b>email</b></label>
+                    <input type="text" placeholder="Vul E-mail in.." name="email" required><br>
+
+                    <label for="wachtwoord"><b>wachtwoord</b></label>
+                    <input type="password" placeholder="Vul wachtwoord in.." name="wachtwoord" required><br>
+
+                    <label for="telefoonnummer"><b>telefoonnummer</b></label>
+                    <input type="text" placeholder="Vul telefoonnummer in.." name="telefoonnummer" required><br>
+
+                    <label for="geboortedatum"><b>geboortedatum</b></label>
+                    <input type="date" placeholder="Vul geboortedatum in.." name="geboortedatum" required><br>
+
+                    <label for="adres"><b>adres</b></label>
+                    <input type="text" placeholder="Vul adres in.." name="adres" required><br>
+
+                    <label for="postcode"><b>postcode</b></label>
+                    <input type="text" placeholder="Vul postcode in.." name="postcode" required><br>
+
+                    <button id="buttonBestel" type="submit" id="reg-button">Registreren</button>
             </div>
         </div>
     </div>
+    
 </body>
 
 </html>
