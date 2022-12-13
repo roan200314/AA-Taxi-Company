1e788ba7f7f3907821806fc6b661735bbf942226
Roan <roan_alhelly@hotmail.com>
2022-12-13 08:59:41 +0100

    13/12/2022

--- forms/account.php
+++ forms/account.php
@@ -20,7 +20,7 @@ if (!empty($_SESSION['userData'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/Header.css">
+    <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/account.css">
     <title>Fake Taxi: Account</title>
 </head>
--- forms/bestellen.php
+++ forms/bestellen.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bestellen.css">
-    <link rel="stylesheet" href="../css/Header.css">
+    <link rel="stylesheet" href="../css/header.css">
     <title>Fake Taxi: Bestellen</title>
 </head>
 
@@ -19,35 +19,36 @@
             <?php include '../navbar/navbar.php' ?>
         </div>
         <div class="Main">
-            <form action="../behandel/bestellenCheck.php" method="post">
+            <form class="middle" action="../behandel/bestellenCheck.php" method="post">
+                <div class="card-1">
+                    <div class="bestel">
+                        <label for="naam"><b>Naam</b></label>
+                        <input type="text" placeholder=" Vul Naam in.." name="naam" required><br>
 
-                <div class="bestel">
-                    <label for="naam"><b>Naam</b></label>
-                    <input type="text" placeholder=" Vul Naam in.." name="naam" required><br>
+                        <label for="Last name"><b>Telefoonnummer</b></label>
+                        <input type="text" placeholder="Vul telefoonnummer in.." name="telefoonnummer" required><br>
 
-                    <label for="Last name"><b>Telefoonnummer</b></label>
-                    <input type="text" placeholder="Vul telefoonnummer in.." name="telefoonnummer" required><br>
+                        <label for="email"><b>E-mail</b></label>
+                        <input type="text" placeholder="Vul E-mail in.." name="email" required><br>
 
-                    <label for="email"><b>E-mail</b></label>
-                    <input type="text" placeholder="Vul E-mail in.." name="email" required><br>
+                        <label for="Password"><b>Adres</b></label>
+                        <input type="text" placeholder="Vul adres in.." name="adres" required><br>
 
-                    <label for="Password"><b>Adres</b></label>
-                    <input type="text" placeholder="Vul adres in.." name="adres" required><br>
-
-                    <div class="dropdown">
-                        <button class="dropbtn">Chauffeur</button>
-                        <div class="dropdown-content">
-                            <input type="radio" name="check1" value="Khalifa"> Mia Khalifa<br>
-                            <input type="radio" name="check1" value="Sins"> Johnny Sins<br>
-                            <input type="radio" name="check1" value="Danger"> Abella Danger
+                        <div class="dropdown">
+                            <button class="dropbtn">Chauffeur</button>
+                            <div class="dropdown-content">
+                                <input type="radio" name="check1" value="Khalifa"> Mia Khalifa<br>
+                                <input type="radio" name="check1" value="Sins"> Johnny Sins<br>
+                                <input type="radio" name="check1" value="Danger"> Abella Danger
+                            </div>
                         </div>
-                    </div>
 
-                    <button id="buttonBestel" type="submit" id="reg-button">Bestellen</button>
+                        <button id="buttonBestel" type="submit" id="reg-button">Bestellen</button>
+                    </div>
                 </div>
             </form>
+            </div>
         </div>
-    </div>
 </body>
 
 </html>
